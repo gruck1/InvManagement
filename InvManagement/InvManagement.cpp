ace579db9bc665c43bb34e428e92ca7645a23bfa
prosserdaniel <prosserdaniel922@gmail.com>
2026-06-08 14:06:20 +1200

    shlooby

--- InvManagement/InvManagement.cpp
+++ InvManagement/InvManagement.cpp
@@ -386,17 +386,11 @@ static void checkPassword(string& value) {
 		cout << "Error: password must be over 12 characters. Try again: ";
 		cin >> value;
 
-		// Since checkPassword is called inside checkPassword, one return doesn't stop all the functions previously called
 		if (value == "0") {
 			return;
 		}
 
 		checkPassword(value);
-
-		// This is why after checkPassword returns, we need to check for the value that has been modified
-		if (value == "0") {
-			return;
-		}
 	}
 
 	// string::npos means target was not found, we can use this to check for any of the following characters
@@ -411,10 +405,6 @@ static void checkPassword(string& value) {
 		}
 
 		checkPassword(value);
-
-		if (value == "0") {
-			return;
-		}
 	}
 
 	while (value.find_first_of("`~!@#$%^&*()_-=+[]{}\\|;:'\"<,.>/?") == string::npos) {
@@ -428,10 +418,6 @@ static void checkPassword(string& value) {
 		}
 
 		checkPassword(value);
-
-		if (value == "0") {
-			return;
-		}
 	}
 
 }
@@ -458,7 +444,7 @@ static void editingRoster(int& index, int day) {
 	switch (answer) {
 	case 1: {
 
-		cout << "Use 24 hour time format, use a . for minutes e.g. 12.30\nAnything over 2 decimals will round\nType 0 in both prompts to cancel\n\n";
+		cout << "Use 24 hour time format, use a . for minutes e.g. 12.30\nType 0 in both prompts to cancel\n\n";
 
 		cout << "Enter starting hours";
 		if (day != roster[index].size()) {
@@ -580,7 +566,7 @@ static void editingAccounts(int& index, int& value) {
 	switch (value) {
 	case 0:
 	case 1:
-		cout << format("Enter new {} (type 0 to cancel): ", (*targetFormats)[value]);
+		cout << format("Enter new {} (type 0 to go back): ", (*targetFormats)[value]);
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		cin >> newValue;
 
@@ -589,13 +575,13 @@ static void editingAccounts(int& index, int& value) {
 		}
 
 		if (value == 0) {
-			if (newValue.length() < 5) {
+			if (username.length() < 5) {
 				cout << "Error: username must be over 5 characters\n\n";
 				waitForInput();
 				return;
 			}
 
-			if (checkLogin(newValue, password, "create")) {
+			if (checkLogin(username, password, "create")) {
 				cout << "Error: username already exists. Please pick a different one\n\n";
 				waitForInput();
 				return;
@@ -610,12 +596,6 @@ static void editingAccounts(int& index, int& value) {
 			checkPassword(newValue);
 
 			if (newValue == "0") {
-				return;
-			}
-
-			if (accounts[index][1] == newValue) {
-				cout << "Error: password cannot be the same as old one\n\n";
-				waitForInput();
 				return;
 			}
 
@@ -645,7 +625,7 @@ static void editingAccounts(int& index, int& value) {
 
 	vectorToFile();
 
-	cout << format("Successfully updated account {}'s {} from {} to {}\n\n", index + 1, (*targetFormats)[value], prevValue, accounts[index][value]);
+	cout << format("Successfully updated {}'s {} from {} to {}\n\n", accounts[index][0], (*targetFormats)[value], prevValue, accounts[index][value]);
 	waitForInput();
 
 }
@@ -756,7 +736,9 @@ static void editingItem(int& max) {
 	case 0:
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		getline(cin, newName);
+
 		convertToTitle(newName);
+
 		displayNewValue = newName;
 
 		if (newName == "0") {
@@ -789,7 +771,6 @@ static void editingItem(int& max) {
 		else {
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			getline(cin, newRole);
-			convertToTitle(newRole);
 			displayNewValue = newRole;
 
 			if (newRole == "0") {
@@ -899,7 +880,7 @@ static void deleteItem(int& max) {
 		return;
 	}
 
-	cout << "Choose the ID of the item you want to delete (type 0 to cancel): ";
+	cout << "Choose the ID of the item you want to delete (type 0 to go back): ";
 	validateInput(index, 0, max);
 
 	if (index == 0) {
@@ -925,10 +906,6 @@ static void deleteItem(int& max) {
 		}
 
 		(*targetVec).erase((*targetVec).begin() + index);
-
-		if (fileName == "employees.txt") {
-			roster.erase(roster.begin() + index);
-		}
 
 		vectorToFile();
 		getFileInfo(fileInfoNum);
@@ -1039,7 +1016,7 @@ static void addItem() {
 
 	file.open(fileName, ios::app);
 	if (fileName != "employees.txt") {
-		(*targetVec).push_back({ name, format("{}", commaFormat(amount, false)), format("${}", commaFormat(price)) });
+		(*targetVec).push_back({ name, format("{}", commaFormat(amount, false)), format("${}", commaFormat(amount)) });
 		file << format("{}\n{}\n{}\n\n", name, format("{}", commaFormat(amount, false)), format("${}", commaFormat(price)));
 
 		cout << format("\nSuccessfully added {} to {}'s inventory\n\n", name, storeString);
@@ -1269,7 +1246,7 @@ static void editRoster() {
 
 		case 2:
 
-			cout << "Pick employee by ID (type 0 to cancel): ";
+			cout << "Pick employee by ID (type 0 to go back): ";
 			validateInput(index, 0, roster.size());
 
 			if (index == 0) {
@@ -1361,44 +1338,6 @@ static void purchaseProduct() {
 	waitForInput();
 }
 
-static void editAccountDetails() {
-
-	fileInfoNum = 3;
-	getFileInfo(3);
-
-	while (true) {
-
-		system("cls");
-		cout << "\n";
-
-		int index = 0, answer{};
-		string displayDetails{}, prevValue{}, newValue{};
-
-		for (index; index < accounts.size(); index++) {
-			if (username == accounts[index][0]) {
-				break;
-			}
-		}
-
-		displayDetails = displaySpecificItem(index);
-
-		cout << displayDetails;
-		cout << "1. Edit username\n2. Edit password\n3. Back\n\n";
-		cout << "Please choose an option: ";
-		validateInput(answer, 1, 3);
-		
-		switch (answer) {
-		case 1:
-		case 2:
-			editingAccounts(index, answer);
-			break;
-
-		case 3:
-			return;
-		}
-	}
-}
-
 // The actual program, this needed to be in its own function so createFiles and fileToVector would only run once
 static void program() {
 
@@ -1447,7 +1386,7 @@ static void program() {
 			//add new account to list of accounts and export to text file
 		case 2:
 
-			cout << "Please choose a username (type 0 to cancel): ";
+			cout << "Please choose a username (type 0 to go back): ";
 			cin >> username;
 			if (username == "0") {
 				break;
@@ -1467,17 +1406,13 @@ static void program() {
 
 			//check password security
 			cout << "Please enter a password - password must be over 12 characters,\ncontain at least one number, and one special character ";
-			cout << "(type 0 to cancel): ";
+			cout << "(type 0 to go back): ";
 			cin >> password;
 			if (password == "0") {
 				break;
 			}
 
 			checkPassword(password);
-
-			if (password == "0") {
-				break;
-			}
 
 			//save username and password without admin access
 			accounts.push_back({ username, password, "false" });
@@ -1569,9 +1504,9 @@ static void program() {
 		}
 		else {
 
-			cout << "1. View Inventory\n2. Order Product\n3. Edit account details\n4. Logout\n5. Delete Account\n6. Exit\n\n";
+			cout << "1. View Inventory\n2. Order Product\n3. Logout\n4. Delete Account\n5. Exit\n\n";
 			cout << "Select an option: ";
-			validateInput(userChoice, 1, 6);
+			validateInput(userChoice, 1, 5);
 
 			system("cls");
 			cout << "\n";
@@ -1590,21 +1525,16 @@ static void program() {
 				break;
 
 			case 3:
-				targetFormats = &accountFormats;
-				editAccountDetails();
-				break;
-
-			case 4:
 				loggedIn = false;
 				cout << "Successfully logged out\n\n";
 				waitForInput();
 				break;
 
-			case 5:
+			case 4:
 				deleteAccount();
 				break;
 
-			case 6:
+			case 5:
 				exit(0);
 			}
 		}
